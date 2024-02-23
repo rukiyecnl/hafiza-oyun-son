9b327158e572e9f9f1dfa36a7c7277281c2f23d8
Rukiye Canlı <canli.rukiye@std.izu.edu.tr>
2024-02-23 23:20:38 +0300

    son hali guncellendi

---
+++ assets/js/memorygame.js
@@ -0,0 +1,117 @@
+let items;
+const gameBox = document.querySelector('.gameBox');
+const move = document.querySelector(".move");
+// const newGame = document.querySelector(".newGame");
+let sayac = 0;
+
+function createGameArray() {
+    items = [1, 2, 3, 4, 5, 6, 7, 8];
+    // ikiye katla
+    items = [...items, ...items];
+
+    // karistir
+    items.sort(x => Math.random() - 0.5);
+}
+
+// function handleGameBtn() {
+//     if(this.classList.contains('correct')) {
+//         return;
+//     }
+
+//     if(this.classList.contains('active')) {
+//         return;
+//     }
+
+//     if(document.querySelectorAll('.gameBox li.active').length >= 2) {
+//         return;
+//     }
+    
+//     this.classList.add('active');
+//     const activeBtns = document.querySelectorAll('.gameBox li.active');{}
+
+//     let timeoutDuration = 1500;
+//      if(activeBtns.length === 2) {
+//         sayac++;
+//         move.innerHTML = sayac;
+//         if(activeBtns[0].innerText === activeBtns[1].innerText) {
+//             activeBtns[0].classList.add('correct');
+//             activeBtns[1].classList.add('correct');
+//             timeoutDuration = 0;
+//         }
+
+//         setTimeout(function() {
+//             activeBtns[0].classList.remove('active');
+//             activeBtns[1].classList.remove('active');
+//         }, timeoutDuration);
+//     }
+// }
+
+
+function handleGameBtn() {
+    if(this.classList.contains('correct')) {
+        return;
+    }
+
+    if(this.classList.contains('active')) {
+        return;
+    }
+
+    // this.classList.add('active');
+    
+    const activeBtns = document.querySelectorAll(".active");
+
+    if (activeBtns.length === 1) {
+        if (activeBtns[0].innerText === this.innerText) {
+            activeBtns[0].classList.remove("active");
+            activeBtns[0].classList.add("correct");  
+            this.classList.add("correct"); 
+            // console.log(document.querySelectorAll(".correct"));
+
+            showFinish();
+
+
+            return;
+        }
+    }
+
+    if (activeBtns.length >= 2) {
+        for (const activeBtn of activeBtns) {
+            activeBtn.classList.remove("active");
+        } 
+    }
+    this.classList.add("active");
+
+}
+
+function renderGameScreen() {
+    gameBox.innerHTML="";
+    for(const item of items) {
+        gameBox.innerHTML += `<li>${item}</li>`;
+    }
+
+    const gameBtns = document.querySelectorAll('.gameBox li');
+    
+    for (const btn of gameBtns) {
+        btn.addEventListener('click', handleGameBtn);
+    }
+}
+
+function showFinish(){
+    if (document.querySelectorAll("li").length === document.querySelectorAll(".correct").length) {
+        alert("bitti");
+    }
+}
+
+function init() {
+    createGameArray();
+    renderGameScreen();
+}
+init();
+
+document.querySelector(".newGame").addEventListener("click", function(e){
+    e.preventDefault();
+    init();
+})
+
+
+// setTimeout(sayHello, 1500);
